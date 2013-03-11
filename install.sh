a8c0059fa5a499f5ca31038b196fad9e36ca566c
Christian Carey <christian.carey@gmail.com>
2013-03-11 09:39:58 -0700

    add installer script that retrieves all the parts and runs them

---
+++ install.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+BASE_URL="https://raw.github.com/cccarey/gitlab-installer/stepped_install"
+VERSION="el6"
+
+parts=13
+current=1
+
+while [ $current -le $parts ]; do
+    url="$BASE_URL/$VERSION/install-step-`printf "%02d" $current`.sh"
+    wget $url
+    current=$(( $current + 1 ))
+done
+
+current=1
+while [ $current -le $parts ]; do
+    source install-step-`printf "%02d" $current`.sh"
+done
